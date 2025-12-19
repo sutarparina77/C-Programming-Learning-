96dec5883ae06bd6a543758a7b960265eaec2c5a
Parina Ravindra Sutar <sutarparina77@gmail.com>
2025-12-19 08:03:29 +0530

    Initialize Rad and Circum with 0.0 for clarity

--- Basic_Program/12 Circumference of Circle.c
+++ Basic_Program/12 Circumference of Circle.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 int main()
 {
-     float Rad = 0, Circum = 0;
+     float Rad = 0.0, Circum = 0.0;
      
      printf("\n Radius of Circle = ");
      scanf("%f",&Rad);
      
-     Circum =2 *  3.14 * Rad ;
+     Circum = 2 *  3.14 * Rad ;
        
      printf("\n Circumference of Circle is = %0.4f",Circum);
      
