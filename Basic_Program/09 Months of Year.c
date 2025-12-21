14a41ec3813fe049edb76bddcf308ca1c36ad04e
Parina Sutar <sutarparina77@gmail.com>
2025-12-21 12:56:11 +0530

    Add files via upload

---
+++ Basic_Program/09 Months of Year.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Num = 0;
+  
+  printf("\n Enter The Number");
+  scanf("%d",&Num);
+  
+  if(Num == 1)
+  {
+    printf("\n\n The Month is January");
+  }
+  else if(Num == 2)
+  {
+    printf("\n\n The Month is February");
+  }
+  else if(Num == 3)
+  {
+    printf("\n\n The Month is March");
+  }
+  else if(Num == 4)
+  {
+    printf("\n\n The Month is April");
+  }
+  else if(Num == 5)
+  {
+    printf("\n\n The Month is May");
+  }
+  else if(Num == 6)
+  {
+    printf("\n\n The Month is June");
+  }
+  else if(Num == 7)
+  {
+    printf("\n\n The Month is July");
+  }
+  else if(Num == 8)
+  {
+    printf("\n\n The Month is August");
+  }
+  else if(Num == 9)
+  {
+    printf("\n\n The Month is September");
+  }
+  else if(Num == 10)
+  {
+    printf("\n\n The Month is October");
+  }
+  else if(Num == 11)
+  {
+    printf("\n\n The Month is November");
+  }
+  else if(Num == 12)
+  {
+    printf("\n\n The Month is December");
+  }
+  else 
+  {
+    printf("\n\n Invalid Month ");
+  }
+  getch();
+  return 0;
+}
