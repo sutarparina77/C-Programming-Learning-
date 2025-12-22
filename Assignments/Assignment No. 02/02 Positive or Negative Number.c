2120fcfaaa241ef7388a9a161d8cdbe423f13d7a
Parina Sutar <sutarparina77@gmail.com>
2025-12-22 16:43:03 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/02 Positive or Negative Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num = 0;
+    
+    printf("\n Enter The Number");
+    scanf("%d",&Num);
+    
+    if(Num >= 0)
+    {
+        printf("\n Number is Positive");
+    }
+    else
+    {
+        printf("\n Number is Negative");
+    }
+    getch();
+    return 0;
+}
