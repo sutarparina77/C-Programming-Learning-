3a555794b97cce5c082ae46e74f00c38ba3490d8
Parina Sutar <sutarparina77@gmail.com>
2025-12-23 16:44:39 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/16 B Print Table of 25 with Multiplication Reverse .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Count = 10;
+  
+  printf("\n Table of 25: ==> \n\n");
+  
+  while(Count >= 1)
+  {
+    printf("\n %2d * %2d = %3d ", 25 , Count, (25 * Count));
+    Count--;
+  }
+  
+  printf("\n THANKS");
+  
+  getch();
+  return 0;
+}
