0653c05d738e795a2d617826ac90dd7576a4c738
Parina Sutar <sutarparina77@gmail.com>
2025-12-23 16:29:39 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/01 Number from 101 to 110.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Count = 101;
+  
+  while(Count <= 110)
+  {
+    printf("\n %d", Count);
+    Count++;
+  }
+  
+  getch();
+  return 0;
+}
