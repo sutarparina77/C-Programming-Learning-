f15e5cf8c42bbe595fe2e4fda761b0c7752287a0
Parina Sutar <sutarparina77@gmail.com>
2025-12-24 20:19:47 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 01/11 Area of Circle .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     float Rad = 0, Area = 0;
+     
+     printf("\n Radius of Circle = ");
+     scanf("%f",&Rad);
+     
+     Area = 3.14 * Rad * Rad;
+       
+     printf("\n Area of Circle is = %0.4f",Area);
+     
+     getch();
+     return 0;
+}
