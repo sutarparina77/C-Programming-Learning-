fb56c9fd18effa7e54d47e4a982f051be098eb8e
Parina Sutar <sutarparina77@gmail.com>
2025-12-25 17:13:09 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 01/12 Circumference of Circle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     float Rad = 0, Circum = 0;
+     
+     printf("\n Radius of Circle = ");
+     scanf("%f",&Rad);
+     
+     Circum =2 *  3.14 * Rad ;
+       
+     printf("\n Circumference of Circle is = %0.4f",Circum);
+     
+     getch();
+     return 0;
+}
