b2fe87d43d00d501eb5d9cdae0ef7435927cb5f8
Parina Sutar <sutarparina77@gmail.com>
2025-12-26 10:58:41 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/03 C Maximum of 2 Numbers.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num1 = 0, Num2 = 0;
+    
+    up:
+      
+    printf("\n Enter 1st Number ");
+    scanf("%d",&Num1);
+    
+    printf("\n Enter 2nd Number ");
+    scanf("%d",&Num2);
+    
+    if(Num1 == Num2 )
+    {
+        printf("\n Both Numbers are Equal");
+        goto up;
+    }
+    else if(Num1 > Num2 )
+    {
+        printf("\n The Maximum Number is: %d", Num1);
+    }
+    else 
+    {
+        printf("\n The Maximum Number is: %d", Num2);
+    }
+    
+    printf("\n Thanks");
+    
+    getch();
+    return 0;
+ }
