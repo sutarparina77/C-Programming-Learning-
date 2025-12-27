026dcbe3dee524e93f6e726e3eedf40b685d0db7
Parina Sutar <sutarparina77@gmail.com>
2025-12-27 14:05:52 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/07 B Print Letters From E to W.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char Letter ='E';
+
+    printf("\n OUTPUT ==> \n");
+
+    while(Letter <= 'W')
+    {
+        printf(" %c ",Letter);
+        Letter++;
+    }
+    getch();
+    return 0;
+}
