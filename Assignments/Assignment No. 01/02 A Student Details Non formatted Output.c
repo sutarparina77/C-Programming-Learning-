086575fc795014f8f787eca1626621d20fa360a8
Parina Sutar <sutarparina77@gmail.com>
2025-12-29 18:01:55 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 01/02 A Student Details Non formatted Output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("\n\t\t\"Student Information\"...");
+  
+  printf("\n %d %s %d %d %d %d %f", 45,"Rohit", 67, 78, 87, 232, 77.3333);
+  printf("\n %d %s %d %d %d %d %f", 63,"Surya", 72, 68, 83, 223, 74.3333);
+  printf("\n %d %s %d %d %d %d %f", 32,"Ishan", 81, 83, 87, 251, 83.6666);
+ 
+  getch();
+  return 0;
+}
