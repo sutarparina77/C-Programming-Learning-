bbdc5be1c4c1e9d5c9ef5570efbb7f200ac724f5
Parina Sutar <sutarparina77@gmail.com>
2025-12-31 18:47:07 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/10 Print _Hello_ otherwise _ World_.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int No = 0;
+  
+  printf("\n Enter The Number : ");
+  scanf("%d",&No);
+  
+  if(No < 10)
+  {
+    printf("\n\"Hello\"");
+  }
+  else
+  {
+    printf("\n\"World\"");
+  }
+  
+  getch();
+  return 0;
+}
