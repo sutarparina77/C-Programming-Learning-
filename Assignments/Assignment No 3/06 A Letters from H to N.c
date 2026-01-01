0be5aae44dd4951b47fa3d93c85a962831619803
Parina Sutar <sutarparina77@gmail.com>
2026-01-01 18:50:28 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/06 A Letters from H to N.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char ch = 'H';
+  
+   printf("\n OUTPUT ==> \n");
+  
+   while(ch <= 'N')
+   {
+     printf(" %c ", ch);
+     ch++;
+   }
+  
+   getch();
+   return 0;
+}
