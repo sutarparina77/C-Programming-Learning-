4e56215c61f80485e45921777b9bf01c9a76169d
Parina Sutar <sutarparina77@gmail.com>
2026-01-01 18:42:48 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/12 B Print character a_e_i_o_u is Vowel or not in switch.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch = '\0';
+  
+  printf("\n Enter a Character :");
+  scanf("%c",&ch);
+ 
+  switch(ch)
+  {
+    case 'A' :
+    case 'E' :
+    case 'I' :
+    case 'O' :
+    case 'U' :  
+    case 'a' :
+    case 'e' :  
+    case 'i' :  
+    case 'o' :
+    case 'u' :    
+     
+    printf("\n The character %c is Vowel.", ch);
+    break;
+  
+    default :
+  
+    printf("\n The character %c is not Vowel.", ch);
+    break ;
+  }
+  
+  printf("\n Thanks");
+  
+  getch();
+  return 0;
+}
