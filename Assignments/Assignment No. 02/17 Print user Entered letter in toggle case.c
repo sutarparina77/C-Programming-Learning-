d9e82b265c16c9080fa7d7287196e8c5abdb547f
Parina Sutar <sutarparina77@gmail.com>
2026-01-02 18:43:32 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/17 Print user Entered letter in toggle case.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch = '\0';
+  
+  printf("\n Enter a Character (toggle case): ");
+  scanf("%c",&ch);
+  
+  if(ch >= 'A' && ch <= 'Z')
+  {
+    ch = ch + 32;
+    printf("\n %c", ch);
+  }
+  else if(ch >= 'a' && ch <= 'z')
+  {
+    ch = ch - 32;
+    printf("\n %c .", ch);
+  }
+  
+  printf("\n After toggle case : %c\n", ch);
+  
+  getch();
+  return 0;
+}
