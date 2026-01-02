49a93ec167c891cdcf9d8172fb6c9ea38e9f49fe
Parina Sutar <sutarparina77@gmail.com>
2026-01-02 18:25:22 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/14 Print upper case and lower case letter.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch = '\0';
+  
+  printf("\n Enter a Character : ");
+  scanf("%c",&ch);
+  
+  if(ch >= 'A' && ch <= 'Z')
+  {
+    ch = ch + 32;
+    printf("\n Conversion of character to lower case is %c \n", ch);
+  }
+  else if(ch >= 'a' && ch <= 'z')
+  {
+    ch = ch - 32;
+    printf("\n Conversion of character to Upper case is %c \n.", ch);
+  }
+  else
+  {
+    printf("\n The character is not a letter");
+  }
+  
+  getch();
+  return 0;
+}
