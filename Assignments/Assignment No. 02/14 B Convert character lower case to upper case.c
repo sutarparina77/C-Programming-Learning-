86a7e016cc4591b974c668c1f80d876b702e5dd4
Parina Sutar <sutarparina77@gmail.com>
2026-01-02 18:31:20 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/14 B Convert character lower case to upper case.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch = '\0';
+  
+  printf("\n Enter a Character : ");
+  scanf("%c",&ch);
+  
+  if(ch >= 'a' && ch <= 'z')
+  {
+    ch = ch - 32;
+    printf("\n Before conversion of character to Upper case is %c \n", ch);
+  }
+  else
+  {
+    printf("\n After conversion of character to Upper case is %c \n", ch);
+  }
+  
+  getch();
+  return 0;
+}
