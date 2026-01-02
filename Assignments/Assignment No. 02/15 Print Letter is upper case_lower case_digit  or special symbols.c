7a7c3e5b07bcd520bdd31ada7ec7c4fab5737f7b
Parina Sutar <sutarparina77@gmail.com>
2026-01-02 18:38:10 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 02/15 Print Letter is upper case_lower case_digit  or special symbols.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch ='\0';
+  
+  printf("\n Enter a Character");
+  scanf("%c",&ch);
+  
+  if(ch >= 'A' && ch <= 'Z')
+  {
+    printf("\n The character %c is upper case.",ch);
+  }
+  else if(ch >= 'a' && ch <= 'z')
+  {
+    printf("\n The character %c is lower case.",ch);
+  }
+  else if(ch >= '0' && ch <= '9')
+  {
+    printf("\n The character %c is digit.",ch);
+  }
+  else
+  {
+    printf("\n The character %c is special symbols.",ch);
+  }
+  
+  getch();
+  return 0;
+}
