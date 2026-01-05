267fbdefb8747fb2ac6bc38bba3f57ecb9de5bfe
Parina Sutar <sutarparina77@gmail.com>
2026-01-05 20:53:33 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/14 B Sum of user Entered bills till user Enteres 0.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Bill = 0, Cnt = 0, Bill_Sum = 0;
+  
+  printf("\n Enter Today's bills : \n");
+  
+  while(1)
+  {
+    printf("\n Enter Bill no %d :", Cnt + 1);
+    scanf("%d",&Bill);
+    
+    if(Bill == 0)
+    {
+      break;
+    }
+    
+    Bill_Sum = Bill + Bill_Sum;
+    Cnt++;
+  }
+  
+  printf("\n Sum of %d bills is = %d.", Cnt, Bill_Sum);
+  
+  printf("\n Thanks for visiting");
+  
+  getch();
+  return 0;
+}
