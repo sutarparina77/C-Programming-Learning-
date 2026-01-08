2d9e85d9e1c9f7d81fa9d53d4aff9f4b10a42a46
Parina Sutar <sutarparina77@gmail.com>
2026-01-08 16:18:54 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/19 B Print Table of user Entered variable range.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, Cnt = 0, sPt = 0, ePt =0;
+  
+  printf("\n Enter Starting number:");
+  scanf("%d",&sPt);
+  
+  printf("\n Enter ending number:");
+  scanf("%d",&ePt);
+  
+  if(sPt > ePt)
+  {
+    //swap logic
+    sPt += ePt;
+    ePt = sPt - ePt;
+    sPt -= ePt;
+    
+    for(i = 1; i <= 10; i++)
+    {
+        for(j = sPt; j <= ePt; j++)
+        {
+          printf(" %3d ", i*j);
+        }
+        printf(" \n ");
+    }
+   
+   }
+  
+  getch();
+  return 0;
+}
