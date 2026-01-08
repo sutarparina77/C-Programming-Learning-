14f5a075c05332fff8c894de19dd03577aea18af
Parina Sutar <sutarparina77@gmail.com>
2026-01-08 14:02:22 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/18 A Print Table from 27 to 19.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, Cnt = 0;
+  
+  printf("\n Tables from 27 to 19 is : \n");
+  
+  for(i = 1; i <= 10; i++)
+  {
+      for(j = 27; j >= 19; j--)
+      {
+        printf(" %3d ", i*j);
+      }
+      printf("\n");
+  }
+  
+  getch();
+  return 0;
+}
