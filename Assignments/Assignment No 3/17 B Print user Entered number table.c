9e041b105826868c54705508084753ed92b40db5
Parina Sutar <sutarparina77@gmail.com>
2026-01-08 13:58:23 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/17 B Print user Entered number table.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, Cnt = 0, sPt = 0, ePt =0;
+  
+  printf("\n Enter Starting number:");
+  scanf("%d",&sPt);
+  
+  printf("\n Enter ending number:");
+  scanf("%d",&ePt);
+  
+  for(i = 1; i <= 10; i++)
+  {
+      for(j = sPt; j <= ePt; j++)
+    {
+      printf(" %3d ", i*j);
+    }
+    printf(" \n ");
+  }
+  
+  getch();
+  return 0;
+}
