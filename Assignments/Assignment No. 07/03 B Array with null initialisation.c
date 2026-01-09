9912f7522fe5a20f69be6b8664ec9ee7f59c3503
Parina Sutar <sutarparina77@gmail.com>
2026-01-09 12:15:40 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/03 B Array with null initialisation.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Num[7] = {};
+  
+  printf("\n Enter an 7 Elements");
+  
+  printf("\n Value of 1st Element : %d", Num[0]);
+  printf("\n Value of 2nd Element : %d", Num[1]);
+  printf("\n Value of 3rd Element : %d", Num[2]);
+  printf("\n Value of 4th Element : %d", Num[3]);
+  printf("\n Value of 5th Element : %d", Num[4]);
+  printf("\n Value of 6th Element : %d", Num[5]);
+  printf("\n Value of 7th Element : %d", Num[6]);
+ 
+  getch();
+  return 0;
+}
