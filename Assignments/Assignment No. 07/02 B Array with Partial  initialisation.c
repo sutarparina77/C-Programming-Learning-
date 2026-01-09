212ed5825d1419ce8bc3cbdcbb02a6da7a589482
Parina Sutar <sutarparina77@gmail.com>
2026-01-09 12:14:52 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/02 B Array with Partial  initialisation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Num[5] = {23,34,56};
+  
+  printf("\n Enter an 5 Elements");
+  
+  printf("\n Value of 1st Element : %d", Num[0]);
+  printf("\n Value of 2nd Element : %d", Num[1]);
+  printf("\n Value of 3rd Element : %d", Num[2]);
+  printf("\n Value of 4th Element : %d", Num[3]);
+  printf("\n Value of 5th Element : %d", Num[4]);
+  
+  getch();
+  return 0;
+}
