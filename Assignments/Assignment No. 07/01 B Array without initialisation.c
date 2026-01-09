2f870d8ca133c52778c6383419cfb966b7b88015
Parina Sutar <sutarparina77@gmail.com>
2026-01-09 12:12:10 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/01 B Array without initialisation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char ch[5];
+  
+  printf("\n Enter an 5 Elements");
+  
+  printf("\n Value of 1st Element : %c", ch[0]);
+  printf("\n Value of 2nd Element : %c", ch[1]);
+  printf("\n Value of 3rd Element : %c", ch[2]);
+  printf("\n Value of 4th Element : %c", ch[3]);
+  printf("\n Value of 5th Element : %c", ch[4]);
+  
+  getch();
+  return 0;
+}
