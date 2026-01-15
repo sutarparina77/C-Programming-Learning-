074996650a9bc0f237fb90ce79254193acf04d1a
Parina Sutar <sutarparina77@gmail.com>
2026-01-15 21:46:25 +0530

    Add files via upload

--- Assignments/Assignment No. 07/05  Array with accept and display Elements by using loop.c
+++ Assignments/Assignment No. 07/05  Array with accept and display Elements by using loop.c
@@ -6,18 +6,18 @@ int main()
   
   printf("\n Enter all 7 Elements");
   
-  for(i = 0; i < 7; i++)
+  for(i = 1; i <= 7; i++)
   {
-    printf("\n Enter Element No %d : ", i+1);
+    printf("\n Enter Element No %d : ", i);
     scanf("%d",&Num[i]);
   }
   
   printf("\n Press Any key to see all 7 values \n");
   getch();
   
-  for(i = 0; i < 7; i++)
+  for(i = 1; i <= 7; i++)
   {
-    printf("\n Value of %d Element : %d", i+1, Num[i]);
+    printf("\n Value of %d Element : %d", i, Num[i]);
   }
   
   getch();
