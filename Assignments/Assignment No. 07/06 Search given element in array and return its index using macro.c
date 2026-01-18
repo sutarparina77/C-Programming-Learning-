e7ba1e9044f0f345fd8f20d652ad49374857a463
Parina Sutar <sutarparina77@gmail.com>
2026-01-18 17:57:20 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/06 Search given element in array and return its index using macro.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 7
+int main()
+{
+  int Bill[size] = {}, i = 0, s_Ele = 0;
+  
+  for(i = 0; i < size; i++)
+  {
+    printf("\n Enter bill No %d:",i+1);
+    scanf("%d",&Bill[i]);
+  }
+  
+  printf("\n Enter element to be searched in array :");
+  scanf("%d",&s_Ele);
+  
+  for(i = 0; i < size; i++)
+  {
+    if(Bill[i] == s_Ele)
+    {
+      break;
+    }
+  }
+
+  if(i == size)
+  {
+    printf("\n No this element found in given array");
+  }
+  else
+  {
+    printf("\n element %d found at index %d:", Bill[i], i);
+  }
+ 
+  getch();
+  return 0;
+}
