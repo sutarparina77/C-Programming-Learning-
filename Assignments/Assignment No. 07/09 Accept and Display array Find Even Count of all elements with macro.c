9114c038cb426b4e5012299973be868ad454af99
Parina Sutar <sutarparina77@gmail.com>
2026-01-19 20:44:53 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/09 Accept and Display array Find Even Count of all elements with macro.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 7
+int main()
+{
+  int Bill[Size] = {}, i = 0, E_Cnt = 0;
+  
+  for(i = 0; i < Size; i++)
+  {
+    printf("\n Enter Bill No %d:",i+1);
+    scanf("%d",&Bill[i]);
+  }
+  
+  for(i = 0; i < Size; i++)
+  {
+    if(Bill[i] %2 == 0 )
+    {
+      E_Cnt++;
+    }
+  }
+  
+  printf("\n Even Count of all bills = %d.", E_Cnt);
+  
+  getch();
+  return 0;
+}
