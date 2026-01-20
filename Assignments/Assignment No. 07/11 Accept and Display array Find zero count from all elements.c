70d3d96796fed3073b79317910d523b6fd072791
Parina Sutar <sutarparina77@gmail.com>
2026-01-20 10:24:04 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/11 Accept and Display array Find zero count from all elements.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 7
+int main()
+{
+  int Bill[Size] = {}, i = 0, Z_Cnt = 0;
+  
+  for(i = 0; i < Size; i++)
+  {
+    printf("\n Enter Bill No %d:",i+1);
+    scanf("%d",&Bill[i]);
+  }
+  
+  for(i = 0; i < Size; i++)
+  {
+    if(Bill[i] == 0)
+    {
+      Z_Cnt++;
+    }
+  }
+  
+  printf("\n Zero Count of all bills = %d.", Z_Cnt);
+  
+  getch();
+  return 0;
+}
