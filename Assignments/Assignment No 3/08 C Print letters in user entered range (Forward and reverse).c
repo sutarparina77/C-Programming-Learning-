edcc546c453cd7bb772fbbd0d6640f7b1d3ccf73
Parina Sutar <sutarparina77@gmail.com>
2026-01-22 15:13:32 +0530

    Add files via upload

---
+++ Assignments/Assignment No 3/08 C Print letters in user entered range (Forward and reverse).c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char sPT = '\0', ePT = '\0';
+
+    printf("\n Enter Starting letter :");
+    sPT = getche();
+
+    printf("\n Enter Ending letter :");
+    ePT = getche();
+
+    printf("\n Output ==> \n\n");
+
+    if(sPT <= ePT)
+    {
+          while(sPT <= ePT)
+          {
+            printf(" %c ", sPT);
+            sPT++;
+          }
+    }
+    else
+    {
+          while(sPT >= ePT)
+          {
+            printf(" %c ", sPT);
+            sPT--;
+          }
+    }
+
+    printf("\n\n Thanks");
+
+    getch();
+    return 0;
+}
