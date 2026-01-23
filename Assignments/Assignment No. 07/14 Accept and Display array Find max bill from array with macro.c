130a4f05bc85d1b983cb6fd8c78c3f2294f376c5
Parina Sutar <sutarparina77@gmail.com>
2026-01-23 22:36:48 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/14 Accept and Display array Find max bill from array with macro.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 7
+int main()
+{
+  int Bill[Size] = {}, i = 0, Max_Bill = 0;
+  
+  for(i = 0; i < Size; i++)
+  {
+    printf("\n Enter Bill No %d:",i+1);
+    scanf("%d",&Bill[i]);
+    
+    if((i == 0) || (Bill[i] > Max_Bill))
+    {
+      Max_Bill = Bill[i];
+    }
+  }
+  
+  printf("\n Maximum bill from all bills = %d.", Max_Bill);
+  
+  getch();
+  return 0;
+}
