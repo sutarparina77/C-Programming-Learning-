c3137e76149e36014ca6839cebadea83892e4c94
Parina Sutar <sutarparina77@gmail.com>
2026-02-05 15:01:46 +0530

    Add files via upload

---
+++ Assignments/Structures Assignment/07 Array of student objects with macro.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+struct student
+#define S_Cnt 5
+{
+    int Roll_No;
+    char Name[24];
+    float Marks;
+    char Address[20];
+};
+int main()
+{
+    int i = 0;
+    struct student studlist[S_Cnt] = {};
+
+    for(i = 0; i < S_Cnt; i++)
+    {
+        printf("\n Enter Student %d details ==>",i+1);
+
+        printf("\n Enter Roll Number:");
+        scanf("%d",&studlist[i].Roll_No);
+
+        fflush(stdin);
+        printf("\n Enter a Name:");
+        gets(studlist[i].Name);
+
+        fflush(stdin);
+
+        printf("\n Enter Marks:");
+        scanf("%f",&studlist[i].Marks);
+
+        fflush(stdin);
+        printf("\n Enter Address:");
+        gets(studlist[i].Address);
+    }
+    system("cls");
+    printf("\n Display student details===>");
+    getch();
+
+    for(i = 0; i < S_Cnt; i++)
+    {
+        printf("\n\n %d student info==>",i+1);
+
+        printf("\n Roll Number : %d",studlist[i].Roll_No);
+        printf("\n Name : %s",studlist[i].Name);
+        printf("\n Marks : %f",studlist[i].Marks);
+        printf("\n Address : %s",studlist[i].Address);
+    }
+    getch();
+    return 0;
+}
+
+
+
+
+
+
