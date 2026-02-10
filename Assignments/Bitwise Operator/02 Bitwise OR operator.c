6b1093ba6c5467a1e1c27430d81ad5421f3245ab
Parina Sutar <sutarparina77@gmail.com>
2026-02-10 12:30:34 +0530

    Add files via upload

---
+++ Assignments/Bitwise Operator/02 Bitwise OR operator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int N1 = 0, N2 = 0, Res = 0;
+
+    printf("\n Enter 1st number:");
+    scanf("%d",&N1);
+
+    printf("\n Enter 2nd number:");
+    scanf("%d",&N2);
+
+    Res = N1 | N2;
+
+    printf("\n Result of %d OR %d : %d", N1, N2, Res);
+
+    getch();
+    return 0;
+}
