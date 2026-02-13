1c345a6934be5d915b43f0c9d636e0692da4f74e
Parina Sutar <sutarparina77@gmail.com>
2026-02-13 14:07:50 +0530

    Add files via upload

---
+++ Assignments/Bitwise Operator/Bitwise NOT operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int N1 = 0, Res = 0;
+
+    printf("\n Enter 1st number:");
+    scanf("%d",&N1);
+
+    Res = ~ N1;
+
+    printf("\n Result of NOT %d : %d", N1, Res);
+
+    getch();
+    return 0;
+}
