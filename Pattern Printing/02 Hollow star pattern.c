a463d782d46378cfca7ce3ba632d346abcdd9521
Parina Sutar <sutarparina77@gmail.com>
2026-02-16 08:48:29 +0530

    Add files via upload

---
+++ Pattern Printing/02 Hollow star pattern.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, R = 0, C = 0;
+  
+  printf("\n Enter Row size:");
+  scanf("%d",&R);
+  
+  printf("\n Enter Column size:");
+  scanf("%d",&C);
+  
+  for(i = 1; i <= R; i++)    
+  {
+    for(j = 1; j <= C; j++)   
+    {
+      if(i == 1 || i == R || j == 1 || j == C)
+      {
+          printf(" * ");
+      }
+      else
+      {
+          printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  getch();
+  return 0;
+}
