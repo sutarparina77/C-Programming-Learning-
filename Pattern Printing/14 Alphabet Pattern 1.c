633f68c55f2238a941b2fefe304e550c5c923d82
Parina Sutar <sutarparina77@gmail.com>
2026-03-09 20:39:50 +0530

    Add files via upload

---
+++ Pattern Printing/14 Alphabet Pattern 1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, R = 0, C = 0;
+  char ch = 'A';
+  
+  printf("\n Enter Row size:");
+  scanf("%d",&R);
+    
+  printf("\n Enter Column size:");
+  scanf("%d",&C);
+  
+  for(i = 1; i <= R; i++)
+  {
+    for(j = 1; j <= C; j++)   
+    {
+          printf(" %c ", ch);
+          ch++;
+    }
+    printf("\n");
+  }
+  
+  getch();
+  return 0;
+}
