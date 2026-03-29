ad6c182d09860a0eff1c992ff65f31a68454d2af
Parina Sutar <sutarparina77@gmail.com>
2026-03-29 20:38:23 +0530

    Add files via upload

---
+++ Assignments/Digits/01 A Sum of digits on given number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int No = 0, D_Sum = 0, Dig = 0, Temp = 0;
+  
+  printf("\n Enter a Number for sum of their digits:");
+  scanf("%d",&No);
+  
+  Temp = No;
+  
+  while(Temp != 0)
+  {
+    Dig = Temp % 10;
+    D_Sum += Dig;
+    Temp /= 10;
+  }
+  printf("\n Sum of digits of given number %d = %d", No, D_Sum);
+ 
+  getch();
+  return 0;
+}
