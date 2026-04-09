9ddec4e51f96e3edf8f5dcf2f7dc2be00068f17e
Parina Sutar <sutarparina77@gmail.com>
2026-04-09 23:16:40 +0530

    Add files via upload

---
+++ Pattern Printing/21 Arrow pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, RC = 0;
+  
+  printf("\n Enter Rows and Column size :");
+  scanf("%d",&RC);
+   
+  for(i = 1; i <= RC; i++)
+  {
+    for(j = 1; j <= RC; j++)
+    {
+      if(j == RC/2 + 1 || i + j == (RC/2 + 1) + 1 || 
+        j == RC/2 + i)
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+     printf("\n");
+  }
+  
+  getch();
+  return 0;
+}
