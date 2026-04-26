a1172e87e60954a48d66ea241e6c723a2f9030d2
Parina Sutar <sutarparina77@gmail.com>
2026-04-26 22:53:25 +0530

    Add files via upload

---
+++ Pattern Printing/Pattern Assignment/04 Pattern.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0, j = 0, RC = 0;
+    char ch = '\0';
+
+    printf("\n Enter Row and Column Size: ");
+    scanf("%d",&RC);
+
+    for(ch = 'A', i = 1; i <= RC; i++, ch++)
+    {
+        for(j = 1; j <= RC; j++)
+        {
+            if(j == 1 || i == RC || i >= j)
+            {
+                printf(" %c ", ch);
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+
+}
