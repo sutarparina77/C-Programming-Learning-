0346f2f1f3d67723823985c47e83913a60bab1d2
Parina Sutar <sutarparina77@gmail.com>
2026-05-28 23:12:54 +0530

    Add files via upload

---
+++ Assignments/Pointer/Pointer Arithmetic/05 Pointer Arithmetic - Multiply by integral constant.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num[5] = {10,20,30,40,50};
+    int *Ptr = Num;
+
+    printf("\n Base address of array is = %d", Num);
+    printf("\n Value in pointer is = %d", Ptr);
+    printf("\n Value of array element where pointer pointing : %d\n", *Ptr);
+
+    getch();
+
+    Ptr = Ptr * 3;                                            ///Not Allowed
+
+    printf("\n New value in pointer is = %d", Ptr);
+    printf("\n Value of array element where pointer pointing : %d", *Ptr);
+
+    getch();
+    return 0;
+}
+
