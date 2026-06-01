f790be17db882777ef2005725989bfc5a853a25d
Parina Sutar <sutarparina77@gmail.com>
2026-06-01 14:30:14 +0530

    Add files via upload

---
+++ Assignments/Bitwise Operator/13 Program of accept number from user and OFF 7th bit if ON and return modified number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0;
+
+    printf("\n Enter the Number :");
+    scanf("%d",&No);
+
+    No = No & ~(1 << 6);
+
+    printf("\n Modified number of given number : %d", No);
+
+    getch();
+    return 0;
+}
