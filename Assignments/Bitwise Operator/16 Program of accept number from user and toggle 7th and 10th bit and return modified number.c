39792f381650ad0b7e917fba71c214a35b4c89ca
Parina Sutar <sutarparina77@gmail.com>
2026-06-02 23:08:38 +0530

    Add files via upload

---
+++ Assignments/Bitwise Operator/16 Program of accept number from user and toggle 7th and 10th bit and return modified number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No = 0;
+
+    printf("\n Enter the Number :");
+    scanf("%d",&No);
+
+    if(No & (1 << 6))
+    {
+        No = No ^ (1 << 6);
+    }
+
+    if(No & (1 << 9))
+    {
+        No = No ^ (1 << 9);
+    }
+
+    printf("\n Modified number of given number : %d", No);
+
+    getch();
+    return 0;
+}
