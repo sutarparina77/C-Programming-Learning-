936596083238a483e7eda9090fa387253bafb303
Parina Sutar <sutarparina77@gmail.com>
2026-07-11 22:38:58 +0530

    Add files via upload

---
+++ Assignments/Storage Classes/02 Static basic without static keyword.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Book_Read();
+int main()
+{
+    int No = 0;
+
+    No = Book_Read();
+    printf("\n Pages read on the first day = %d", No);
+
+    No = Book_Read();
+    printf("\n Pages read on the Second day = %d", No);
+
+    No = Book_Read();
+    printf("\n Pages read on the Third day = %d", No);
+
+    getch();
+    return 0;
+}
+int Book_Read()
+{
+    int Res = 10;
+    int PgCnt = 0;
+
+    printf("\n\n How many pages read today = ");
+    scanf("%d",&PgCnt);
+
+    Res = Res + PgCnt;
+
+    return Res;
+}
